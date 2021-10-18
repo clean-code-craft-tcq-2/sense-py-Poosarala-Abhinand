3f097536de81919f97c4060dd461159df1978949
Abhinand <90901127+Poosarala-Abhinand@users.noreply.github.com>
2021-10-18 17:49:19 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,11 @@
-
+import math
 def calculateStats(numbers):
-  return None
+  #return computedStats here.
+  # check numbers for math.isnan
+  computedStats = {}
+  computedStats["min"] = math.min(numbers)
+  computedStats["max"] = math.max(numbers)
+  computedStats["avg"] = math.sum(numbers)/math.len(numbers)
+  return computedStats
+
+
