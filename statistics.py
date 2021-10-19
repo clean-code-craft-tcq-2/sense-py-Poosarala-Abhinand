ca8f46fce4ef201dcd150a652b3378b78565dbe7
Abhinand <90901127+Poosarala-Abhinand@users.noreply.github.com>
2021-10-19 10:42:43 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,11 +1,18 @@
 import math
+
 def calculateStats(numbers):
-  #return computedStats here.
+  # return computedStats here.
   # check numbers for math.isnan
   computedStats = {}
-  computedStats["min"] = math.min(numbers)
-  computedStats["max"] = math.max(numbers)
-  computedStats["avg"] = math.sum(numbers)/math.len(numbers)
+  if math.isnan(numbers):
+    computedStats["min"] = float('nan')
+    computedStats["max"] = float('nan')
+    computedStats["avg"] = float('nan')
+  else:
+    computedStats["min"] = math.min(numbers)
+    computedStats["max"] = math.max(numbers)
+    computedStats["avg"] = math.sum(numbers)/math.len(numbers)
   return computedStats
 
-
+class EmailAlert:
+  
