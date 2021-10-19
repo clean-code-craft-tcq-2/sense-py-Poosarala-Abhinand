31fbae52e2390f49cc0b4cade8f2dc82eef79f0e
Abhinand <90901127+Poosarala-Abhinand@users.noreply.github.com>
2021-10-19 11:39:28 +0530

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,14 +1,9 @@
-import math
-
 def calculateStats(numbers):
-  # return computedStats here.
-  # check numbers for math.isnan
   computedStats = {}
-  if numbers  == []:
-    computedStats["min"] = float('nan')
-    computedStats["max"] = float('nan')
-    computedStats["avg"] = float('nan')
-  else:
+  computedStats["min"] = float('nan')
+  computedStats["max"] = float('nan')
+  computedStats["avg"] = float('nan')
+  if numbers:
     computedStats["min"] = min(numbers)
     computedStats["max"] = max(numbers)
     computedStats["avg"] = sum(numbers)/len(numbers)
@@ -26,7 +21,10 @@ class StatsAlerter:
   def __init__(self, maxThreshold, alert):
     self.maxThreshold = maxThreshold
     self.alert = alert
+
   def checkAndAlert(self, values):
-    print(values)
-      
-    
+    for i in values:
+	if i >= self.maxThreshold:
+		self.alert[0].emailSent = True
+                self.alert[1].ledGlows = True
+        break
